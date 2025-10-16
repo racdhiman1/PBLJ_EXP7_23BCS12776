29028e05774b7eb871753fc04a514feb2a01e4f0
racdhiman1 <rachitdhiman1@gmail.com>
2025-10-16 21:44:06 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class Student {
+    private int studentID;
+    private String name;
+    private String department;
+    private double marks;
+
+    public Student(int studentID, String name, String department, double marks) {
+        this.studentID = studentID;
+        this.name = name;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    public int getStudentID() { return studentID; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getMarks() { return marks; }
+
+    public void setMarks(double marks) { this.marks = marks; }
+}
